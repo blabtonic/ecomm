1b06a2dfa6d3f06c88240baf277e961114432785
Trystan Hendricks <dolkhound@gmx.com>
2014-11-11 02:37:59 -0500

    creating User section of app

--- app/models/User.php
+++ app/models/User.php
@@ -21,6 +21,17 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
-
+	protected $hidden = array('password');
+	
+	protected $fillable = array('firstname', 'lastname', 'email', 'telephone');
+	
+	public static $rules = array(
+		'firstname' => 'required|min:2|alpha',
+		'lastname' => 'required|min:2|alpha',
+		'email' => 'required|email|unique:users',
+		'password' => 'required|alpha_num|between:8,12|confirmed',
+		'password_confirmation' => 'required|alpha_num|between8,12',
+		'telephone' => 'required|between:10,12',
+		'admin' => 'integer'
+	);
 }
--- app/routes.php
+++ app/routes.php
@@ -16,3 +16,4 @@ Route::get('/', array('uses' => 'StoreController@getIndex'));
 Route::controller('admin/categories', 'CategoriesController');
 Route::controller('admin/products', 'ProductsController');
 Route::controller('store', 'StoreController');
+Route::controller('users', 'UsersController');
