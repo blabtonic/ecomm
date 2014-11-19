d1a1228d43408844dfdf71d8678469e3bd99afce
Trystan Hendricks <dolkhound@gmx.com>
2014-11-19 01:14:01 -0500

    added the laravel-cart package

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-                'Intervention\Image\ImageServiceProvider'
+                'Intervention\Image\ImageServiceProvider',
+                'Moltin\Cart\CartServiceProvider',
 
 	),
 
@@ -190,6 +191,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
                 'Image'            => 'Intervention\Image\Facades\Image',
+                'Cart'            => 'Moltin\Cart\Facade',
 
 	),
 
